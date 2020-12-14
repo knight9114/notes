e276fcaec22546f37c14a578fb1c4d2124fcf140
knight9114 <38087384+knight9114@users.noreply.github.com>
2020-12-14 18:12:02 -0500

    Update tutorial.sh

--- tutorial.sh
+++ tutorial.sh
@@ -1,87 +1,99 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Bash Trap - catches Ctrl-C
-trap bashtrap INT
-bashtrap() {
-    echo "\nBASH TRAP ACTIVATED"
+set -Eeuo pipefail
+
+cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
+
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+usage() {
+  cat <<EOF
+Usage: $(basename "$0") [-h] [-v] [-f] -p param_value arg1 [arg2...]
+
+Script description here.
+
+Available options:
+
+-h, --help      Print this help and exit
+-v, --verbose   Print script debug info
+-f, --flag      Some flag description
+-p, --param     Some param description
+EOF
+  exit
 }
 
-# Reading Input
-#read -p "Username: " USERNAME
-#read -sp "Password: " PASSWORD
-#echo
-#echo "Username: $USERNAME    Password: $PASSWORD"
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # script cleanup here
+}
 
-# Math
-#a=$(( 4 + 5 ))
-#b=$(( $a + 1 ))
-#c=${#b}          # Length of b --> b==10 --> len(b)==2
+setup_colors() {
+  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+    NOCOLOR='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+  else
+    NOCOLOR='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+  fi
+}
 
-# If Statements
-#if [ $1 -gt 100 ]; then
-    #echo "$1 is a big number"
-#elif [ $1 -lt 100 ]; then
-    #echo "$1 is a small number"
-#else
-    #echo "$1 is 100"
-#fi
+msg() {
+  echo >&2 -e "${1-}"
+}
 
-# Case Statement
-#case $(( $1 % 2 )) in
-    #0)
-        #echo "$1 is even"
-        #;;
-    #1)
-        #echo "$1 is odd"
-        #;;
-    #*)
-        #echo "IMPOSSIBLE!"
-        #;;
-#esac
+die() {
+  local msg=$1
+  local code=${2-1} # default exit status 1
+  msg "$msg"
+  exit "$code"
+}
 
-# Test Operators
-# !EXPRESSION --> Logical Not
-# -n STRING --> Length of the STRING > 0
-# -z STRING --> Length of the STRING == 0, ie: empty
-# STRING1 = STRING2 --> Equality
-# INT1 -eq INT2 --> Equality
-# -[rwx] FILE --> FILE exists and is [readable, writable, executable]
-# -d FILE --> FILE exists and is a directory
-# -e FILE --> FILE exists
-# -s FILE --> FILE exists and is nonempty
+parse_params() {
+  # default values of variables set from params
+  flag=0
+  param=''
 
-# Loops
-#counter=0
-#while [ $counter -lt 10 ]; do
-    #echo $counter
-    #(( counter++ ))
-#done
+  while :; do
+    case "${1-}" in
+    -h | --help)
+      usage
+      ;;
+    -v | --verbose)
+      set -x
+      ;;
+    --no-color)
+      NO_COLOR=1
+      ;;
+    -f | --flag) # example flag
+      flag=1
+      ;;
+    -p | --param) # example named parameter
+      param="${2-}"
+      shift
+      ;;
+    -?*)
+      die "Unknown option: $1"
+      ;;
+    *)
+      break
+      ;;
+    esac
+    shift
+  done
 
-#until [ $counter -eq 10 ]; do
-    #echo $counter
-    #(( counter++ ))
-#done
+  args=("$@")
 
-#for i in {10..0..2}; do
-    #echo $i
-#done
+  # check required params and arguments
+  [[ -z "${param-}" ]] && die "Missing required parameter: param"
+  [[ ${#args[@]} -eq 0 ]] && die "Missing script arguments"
 
-# Piping Example - cat salesdata.txt | ./tutorial.sh
-#cat /dev/stdin | cut -d' ' -f 2,3 | sort
-#echo $(cat /dev/stdin | wc -l)
+  return 0
+}
 
-# Get Logins
-#last | grep "$USER" | grep "$(date +"%a %b %_d")"
+parse_params "$@"
+setup_colors
 
-# Useful Notes
-# Press Ctrl-Alt-F<1-7> to change consoles
-#    Normally, we use the GUI F7
-#    If something locks up, press Ctrl-Alt-F2 to get other console
+# script logic here
 
-# Xargs Example - Create backup files for directory
-#ls | xargs -n1 -i cp {} {}.bak    # -n1: apply command to each individual item
-                                  ## -i: use a replacement string, defaults to {}
-#rm *.bak
-
-# Find Example - Find all files over 200Mb
-find $PWD -size +200M -exec ls -sh {} \;
+msg "${RED}Read parameters:${NOCOLOR}"
+msg "- flag: ${flag}"
+msg "- param: ${param}"
+msg "- arguments: ${args[*]-}"
